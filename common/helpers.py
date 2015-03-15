285c5cff8efab83ccc9afebcbd23ae17335224a9
Chris Truncer <chris@christophertruncer.com>
2015-03-15 02:37:17 -0400

    Updates to Hasher

---
+++ common/helpers.py
@@ -0,0 +1,29 @@
+'''
+This part of Hasher contains misc. functions for use throughout
+the tool.
+'''
+
+import os
+
+# Taken from veil-evasion
+def color(string, status=True, warning=False, bold=True):
+    """
+    Change text color for the linux terminal, defaults to green.
+    Set "warning=True" for red.
+    """
+    attr = []
+    if status:
+        # green
+        attr.append('32')
+    if warning:
+        # red
+        attr.append('31')
+    if bold:
+        attr.append('1')
+    return '\x1b[%sm%s\x1b[0m' % (';'.join(attr), string)
+
+def print_header():
+    os.system("clear")
+    print "#" * 80
+    print "#" + " " * 36 + "Hasher" + " " * 36 + "#"
+    print "#" * 80 + "\n"
---
+++ common/orchestra.py
@@ -0,0 +1,97 @@
+'''
+This file is the conductor of everything Hasher.
+'''
+
+import argparse
+import glob
+import imp
+import sys
+from common import helpers
+from hash_ops import *
+
+
+class Conductor:
+    def __init__(self):
+            # default values of object attributes
+            self.username = ''
+            self.password = ''
+            self.hash_value = ''
+            self.hash_type = ''
+            self.rounds = 5000
+            self.salt = ''
+            self.action = ''
+
+            # Dictionary containing hashing algorithms
+            self.hashing_algorithms = {}
+
+    def cliParser(self):
+        parser = argparse.ArgumentParser(description="Create or Verify hashes with plaintext strings.")
+        parser.add_argument("--list", action="store_true", default=False, help="List all supported hash algorithms")
+        parser.add_argument("-G", default=False, help="Generate a hash from the provided string.", action='store_true')
+        parser.add_argument("-C", default=False, help="Compare provided plaintext with a hash", action='store_true')
+        parser.add_argument("--hash-type", metavar="md5", default=None, help="The hashing algorithm you want to use")
+        parser.add_argument("--plaintext", metavar="password", default=None, help="Plaintext string to hash")
+        parser.add_argument("--hash", metavar="HASH", default=None, help="Hash used for comparison")
+        parser.add_argument("--rounds", metavar="5000", help="Number of rounds to hash your plaintext string")
+        parser.add_argument("--salt", metavar="SALT", help="Salt used for hashing")
+        parser.add_argument("--username", metavar="USERNAME", help="Only required for select hash types")
+        args = parser.parse_args()
+        if not args.G and not args.C and not args.list:
+            print helpers.color("\n\n[*] Error: You must provide an action to perform", warning=True)
+            print helpers.color("[*] Actions: [G]enerate or [C]ompare hashes.", warning=True)
+            print helpers.color("[*] Please provide an action and re-run Hasher.", warning=True)
+            sys.exit()
+
+        if args.G and (args.plaintext is None or args.hash_type is None) :
+            print helpers.color("\n\n[*] Error: You must provde a plaintext string and hashing algorithm to use!", warning=True)
+            print helpers.color("[*] Please re-run and provide a plaintext string and/or hash algorithm.", warning=True)
+            sys.exit()
+
+        if args.C and (args.plaintext is None or args.hash_type is None or args.hash is None):
+            print helpers.color("\n\n[*] Error: Comparison function requires plaintext string, hash digest, and hash algorithm.", warning=True)
+            print helpers.color("[*] Please re-run and provide the required options", warning=True)
+            sys.exit()
+        return args
+
+    def load_hash_operations(self):
+        for name in glob.glob('hash_ops/*.py'):
+            if name.endswith(".py") and ("__init__" not in name):
+                loaded_algo = imp.load_source(
+                    name.replace("/", ".").rstrip('.py'), name)
+                self.hashing_algorithms[name] = loaded_algo.Algorithm()
+        return
+
+    def menu_system(self):
+
+        # Parse the command line arguments and load up hashing modules
+        cli_args = self.cliParser()
+        self.load_hash_operations()
+
+        # If listing payloads, do it and then exit
+        if cli_args.list:
+            print "Supported Hashing Algorithms: "
+            for path, hash_obj in self.hashing_algorithms.iteritems():
+                print "[*] " + hash_obj.hash_type
+            sys.exit()
+
+        # Generate a hash digest for the provided plaintext string
+        # Iterates over all loaded hash_op modules, finds the hash type
+        # generates the digest, and returns it
+        if cli_args.G:
+            for path, hash_obj in self.hashing_algorithms.iteritems():
+                if cli_args.hash_type.lower().strip() == hash_obj.hash_type.lower():
+                    hashed_string = hash_obj.generate(cli_args)
+                    print "Hash Type        : " + helpers.color(cli_args.hash_type)
+                    print "Plaintext string : " + helpers.color(cli_args.plaintext)
+                    print "Digest Value     : " + helpers.color(hashed_string)
+
+        if cli_args.C:
+            for path, hash_obj in self.hashing_algorithms.iteritems():
+                if cli_args.hash_type.lower().strip() == hash_obj.hash_type.lower():
+                    hashed_string = hash_obj.generate(cli_args)
+                    if hashed_string == cli_args.hash.lower().strip():
+                        print helpers.color("True - The hash " + cli_args.hash + " and plaintext " + cli_args.plaintext + " match!")
+                    else:
+                        print helpers.color("False - The hash " + cli_args.hash + " and plaintext " + cli_args.plaintext + " do not match!", warning=True)
+
+        return
